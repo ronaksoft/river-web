dbb62d9850638ad1a3edc08931a660f4840b9c39
Hamidreza KK <hamidrezakks@gmail.com>
2018-09-25 17:15:12 +0330

    sign up page added

--- src/app/pages/SignUp/index.tsx
+++ src/app/pages/SignUp/index.tsx
@@ -52,15 +52,19 @@ class Chat extends React.Component<IProps, IState> {
                             {/*<input className="input100" type="text" placeholder="Username"/>*/}
                             {/*<span className="focus-input100"/>*/}
                             {/*</div>*/}
-                            {/*<div className="wrap-input100 validate-input m-b-16">*/}
-                                {/*<input className="input100" type="password" placeholder="Password"/>*/}
-                                {/*<span className="focus-input100"/>*/}
-                            {/*</div>*/}
-                            {!this.state.confirmCode && <div className="container-login100-form-btn m-t-17">
+                            {this.state.confirmCode && <div className="wrap-input100 validate-input m-b-16">
+                                <input className="input100" type="text" placeholder="Code"/>
+                            </div>}
+                            <div className="container-login100-form-btn m-t-17">
+                                {!this.state.confirmCode &&
                                 <button className="login100-form-btn" onClick={this.sendCode}>
                                     Send Code
-                                </button>
-                            </div>}
+                                </button>}
+                                {this.state.confirmCode &&
+                                <button className="login100-form-btn" onClick={this.confirmCode}>
+                                    Confirm Code
+                                </button>}
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -87,6 +91,10 @@ class Chat extends React.Component<IProps, IState> {
         if (e.key === 'Enter') {
             this.sendCode();
         }
+    }
+
+    private confirmCode = () => {
+        window.console.log('erer');
     }
 }
 
