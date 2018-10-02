b93d37eb9fd8c67551e68fe79a6a57b76147b69b
Hamidreza Kazemi <hamidrezakks@gmail.com>
2018-10-02 23:04:20 +0330

    dockerfile and nginx added

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,15 @@
-FROM node:8.2.1-alpine
+FROM alpine
 
 # Create app directory
-RUN mkdir -p /ronak/nested
-WORKDIR /ronak/nested
+RUN mkdir -p /ronak/app
+WORKDIR /ronak/app
 RUN apk update
-RUN apk add gettext
 RUN apk add nginx
-RUN npm install -g local-web-server
 EXPOSE 80
 EXPOSE 443
 
-COPY ./bin/nested-reconfig.js /bin/nested-reconfig.js
-COPY ./bin/dns-discovery.js /bin/dns-discovery.js
-COPY ./bin/nginx.conf.template /bin/nginx.conf.template
-COPY ./bin/nginx-ssl.conf.template /bin/nginx-ssl.conf.template
 COPY ./run.sh .
 RUN chmod +x run.sh
 CMD  /bin/sh run.sh
 
-# Install app dependencies
-COPY ./build/webapp /ronak/nested/webapp
+COPY ./build /ronak/app
--- run.sh
+++ run.sh
@@ -1,40 +1,9 @@
 #!/usr/bin/env bash
 
-chown -R nginx:nginx /ronak/nested/webapp
+chown -R nginx:nginx /ronak/app
 chown -R nginx:nginx /var/lib/nginx
 
-echo "Starting Web Servers "
-node /bin/dns-discovery.js &
-
-cd /ronak/nested/webapp
-echo "Directory changed to (`pwd`)"
-node /bin/nested-reconfig.js script=scripts tmp=nestedConfig
-node /bin/nested-reconfig.js script=m/js tmp=nestedConfigMobile
-node /bin/nested-reconfig.js script=admin tmp=nestedConfigAdmin
-node /bin/nested-reconfig.js script=t tmp=nestedConfigT
-node /bin/nested-reconfig.js script=oauth tmp=nestedConfigOauth
-sleep 1
-cd /bin
-
-export DOLLAR='$';
-
-if [ -n "${NST_ADDR_PORT}" ]; then
-    echo "";
-else
-    export NST_ADDR_PORT=80;
-fi
-if  [[ -n "${NST_TLS_KEY_FILE}" && -n "${NST_TLS_CERT_FILE}" ]] ; then
-     if  [[ -f $NST_TLS_CERT_FILE && -f $NST_TLS_KEY_FILE ]]; then
-        echo "Webapp started over SSL" ;
-        envsubst < nginx-ssl.conf.template > /etc/nginx/nginx.conf;
-     else
-        echo "Webapp started without SSL" ;
-        envsubst < nginx.conf.template > /etc/nginx/nginx.conf;
-     fi ;
-else
-     echo "Webapp started without SSL" ;
-     envsubst < nginx.conf.template > /etc/nginx/nginx.conf;
-fi
+mv /ronak/app/nginx.conf /etc/nginx/nginx.conf
 
 nginx -t
 nginx -g "daemon off;"
