ebb49d6d4630f54af02989b2a287063f4ad929c7
Hamidreza Kazemi <hamidrezakks@gmail.com>
2018-10-11 08:55:02 +0330

    udpated

--- public/bin/binding.js
+++ public/bin/binding.js
@@ -128,7 +128,7 @@ function Uint8ToBase64(u8a) {
 
 const initWebSocket = () => {
     // socket = new WebSocket('ws://192.168.1.110');
-    if (window.location.protocol === 'https') {
+    if (window.location.protocol === 'https:') {
         socket = new WebSocket('wss://' + window.location.host + '/ws');
     } else {
         socket = new WebSocket('ws://new.river.im');
--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ class App extends React.Component {
     public componentDidMount() {
         // @ts-ignore
         if (window.Notification) {
-            Notification.requestPermission().then((result) => {
-                window.console.log(result);
-            });
+            Notification.requestPermission();
         }
     }
 
