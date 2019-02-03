09935a0f247af3a620f5e84a606c71b5e18816ca
Hamidrezakk <hamidrezakks@gmail.com>
2019-02-03 10:59:38 +0330

    version udpated

--- src/app/components/SettingMenu/index.tsx
+++ src/app/components/SettingMenu/index.tsx
@@ -205,7 +205,7 @@ class SettingMenu extends React.Component<IProps, IState> {
                             </div>
                         </div>
                         <div className="version">
-                            v0.23.13
+                            v0.23.14
                         </div>
                     </div>
                     <div className="page page-2">
