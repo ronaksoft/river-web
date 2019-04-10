c20fe217b149ab7b76687b77af53b040d2ae839f
Hamidrezakk <hamidrezakks@gmail.com>
2019-04-10 18:17:48 +0430

    version updated

--- src/app/components/SettingMenu/index.tsx
+++ src/app/components/SettingMenu/index.tsx
@@ -64,7 +64,7 @@ import {findIndex} from 'lodash';
 import './style.css';
 import 'react-image-crop/dist/ReactCrop.css';
 
-export const C_VERSION = '0.23.100';
+export const C_VERSION = '0.23.101';
 
 interface IProps {
     onClose?: () => void;
