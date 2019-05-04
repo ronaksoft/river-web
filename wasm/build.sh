9d70571f0ed03b0a9da8268c9bea8287de557586
Hamidrezakk <hamidrezakks@gmail.com>
2019-05-04 10:18:52 +0430

    new wasm

--- wasm/build.sh
+++ wasm/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o ../public/bin/river.wasm src/*.go
+GOOS=js GOARCH=wasm GODEBUG=gcstoptheworld=1 GOGC=20 go build -ldflags="-s -w" -o ../public/bin/river.wasm src/*.go
