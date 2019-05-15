df88bd026dbe7fb164ff4ae74f9d75c0fa732248
Hamidrezakk <hamidrezakks@gmail.com>
2019-05-15 17:52:53 +0430

    fix

--- src/app/services/sdk/server/index.tsx
+++ src/app/services/sdk/server/index.tsx
@@ -72,7 +72,6 @@ export default class Server {
                 this.isConnected = true;
                 this.flushSentQueue();
                 this.executeSendThrottledRequestThrottle();
-                this.updateManager.idleHandler();
             });
 
             window.addEventListener('wsClose', () => {
