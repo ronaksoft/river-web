c2a0b4d01cfac1cb70cb812f5bbf856459813dc4
Hamidreza KK <hamidrezakks@gmail.com>
2021-03-17 17:01:13 +0330

    last seen modified

--- src/app/components/LastSeen/index.tsx
+++ src/app/components/LastSeen/index.tsx
@@ -110,9 +110,7 @@ class LastSeen extends React.PureComponent<IProps, IState> {
             const lastSeen = user.status_last_modified || user.lastseen || 0;
             if (this.state.you || (this.riverTime.now() - lastSeen < 60 && user.status === UserStatus.USERSTATUSONLINE)) {
                 return (<span className="online">{i18n.t('status.online')}</span>);
-            } else if ((user.status === undefined || user.status === UserStatus.USERSTATUSRECENTLY) && user.status_last_modified === 0) {
-                return `${this.props.withLastSeen ? i18n.t('status.last_seen') : ''} ${i18n.t('status.recently')}`;
-            } else if (!lastSeen) { // TODO: server patch
+            } else if (user.lastseen === 0 || !lastSeen) {
                 return `${this.props.withLastSeen ? i18n.t('status.last_seen') : ''} ${i18n.t('status.recently_2')}`;
             } else if (this.lastSeenFormat === 'estimated') {
                 return `${this.props.withLastSeen ? i18n.t('status.last_seen') : ''} ${TimeUtility.timeAgo(lastSeen)}`;
@@ -123,6 +121,9 @@ class LastSeen extends React.PureComponent<IProps, IState> {
     }
 
     private getIntervalTime(time: number) {
+        if (!time) {
+            return -1;
+        }
         const diff = this.riverTime.now() - time;
         if (diff < 86400) {
             return 10000;
