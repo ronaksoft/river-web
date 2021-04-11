d03973ed1f2b83136f61e2fb914596241ff282eb
Hamidreza KK <hamidrezakks@gmail.com>
2021-04-11 16:00:38 +0430

    add space after mention

--- src/app/components/MentionInput/index.tsx
+++ src/app/components/MentionInput/index.tsx
@@ -168,7 +168,7 @@ class MentionInput extends React.Component<IProps, IState> {
                     (reg.test(`${participant.firstname} ${participant.lastname}`) ||
                         (participant.username && reg.test(participant.username)))) {
                     users.push({
-                        display: participant.username ? `@${participant.username}` : `${participant.firstname} ${participant.lastname}`,
+                        display: participant.username ? `@${participant.username} ` : `${participant.firstname} ${participant.lastname} `,
                         id: participant.userid,
                         index: i,
                         username: participant.username,
