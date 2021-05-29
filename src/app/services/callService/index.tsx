6eea1fffc87fc860402501af57690efe52ad89d4
Hamidreza KK <hamidrezakks@gmail.com>
2021-05-29 15:09:39 +0430

    updated

--- src/app/services/callService/index.tsx
+++ src/app/services/callService/index.tsx
@@ -1006,8 +1006,6 @@ export default class CallService {
             usernameFragment: actionData.usernamefragment,
         });
 
-        window.console.log(iceCandidate.toJSON());
-
         conn[connId].connection.addIceCandidate(iceCandidate).catch((err) => {
             window.console.log('cannot add ice candidate', err);
         });
