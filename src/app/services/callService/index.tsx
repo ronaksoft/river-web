34ca70968959cb00844256b91d0c41ea521e6c49
Hamidreza KK <hamidrezakks@gmail.com>
2021-06-16 16:28:55 +0430

    updated

--- src/app/services/callService/index.tsx
+++ src/app/services/callService/index.tsx
@@ -2166,9 +2166,6 @@ export default class CallService {
         if (!this.callInfo.hasOwnProperty(this.activeCallId)) {
             return;
         }
-        if (this.callInfo[this.activeCallId].acceptedParticipants.indexOf(connId) > -1) {
-            return;
-        }
         this.callInfo[this.activeCallId].acceptedParticipants.push(connId);
     }
 
