80f93d284d49c7e469805453e91fafef631ae352
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2024-02-05 01:43:40 +0330

    chore: refactor devops

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ chown -R nginx:nginx /ronak/app
 chown -R nginx:nginx /var/lib/nginx
 
 export DOLLAR='$';
-
+echo "API URL: ${NST_API_URL}"
+echo "FILE_URL: ${NST_FILE_URL}"
 if  [[ -n "${NST_TLS_KEY_FILE}" && -n "${NST_TLS_CERT_FILE}" ]] ; then
      if  [[ -f $NST_TLS_CERT_FILE && -f $NST_TLS_KEY_FILE ]]; then
         echo "Started over SSL" ;
